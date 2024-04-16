a33fa506a189c4e2f844544afca4a3379e799006
Germán Salina <gersalina28@gmail.com>
2024-04-16 16:40:05 -0300

    Update 13_if_condicionales.py

--- 1_FUNDAMENTOS PYTHON/13_if_condicionales.py
+++ 1_FUNDAMENTOS PYTHON/13_if_condicionales.py
@@ -4,7 +4,7 @@ if True:
 if False:
   print('nunca se ejecuta')
 
-number = int(input('Ingrese un numero => '))
+number = int(input('Ingrese un número => '))
 result = number % 2 #Nos devuelve el resto de la división de number sobre 2
 if (result == 0):
 	print('Es par')
