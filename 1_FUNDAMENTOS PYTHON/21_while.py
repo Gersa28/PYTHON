835214074c4972ef2edfd217c879a9591763c442
Germán Salina <gersalina28@gmail.com>
2024-04-16 18:50:34 -0300

    Update 21_while.py

--- 1_FUNDAMENTOS PYTHON/21_while.py
+++ 1_FUNDAMENTOS PYTHON/21_while.py
@@ -1,28 +1,18 @@
-'''
-while True:
-  print('se ejecuto')
-
-
 counter = 0
-
 while counter < 10:
   counter += 1
   print(counter)
 
-
 counter = 0
-
-while counter < 20:
-  counter += 1
-  if counter == 15:
-    break
-  print(counter)
-'''
-
-counter = 0
-
 while counter < 20:
   counter += 1
   if counter < 15:
     continue
+  print(counter)
+  
+counter = 0
+while counter < 20:
+  counter += 1
+  if counter == 15:
+    break
   print(counter)
