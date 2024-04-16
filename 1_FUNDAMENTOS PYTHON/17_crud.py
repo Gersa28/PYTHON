4430d9ecd3cd826d42637c3c3822f671838add23
Germán Salina <gersalina28@gmail.com>
2024-04-16 17:45:50 -0300

    Update 17_crud.py

--- 1_FUNDAMENTOS PYTHON/17_crud.py
+++ 1_FUNDAMENTOS PYTHON/17_crud.py
@@ -1,5 +1,17 @@
 # CRUD Create, Read, Update & Delete
 # Métodos para listas
+"""
+append(): Añade un ítem al final de la lista.
+clear(): Vacía todos los ítems de una lista.
+extend(): Une una lista a otra.
+count(): Cuenta el número de veces que aparece un ítem.
+index(): Devuelve el índice en el que aparece un ítem (error si no aparece).
+insert(): Agrega un ítem a la lista en un índice específico.
+pop(): Extrae un ítem de la lista y lo borra.
+remove(): Borra el primer ítem de la lista cuyo valor concuerde con el que indicamos.
+reverse(): Le da la vuelta a la lista actual.
+sort(): Ordena automáticamente los ítems de una lista por su valor de menor a mayor.
+"""
 
 numbers = [1, 2 , 3 , 4 , 5]
 print(numbers[1])
