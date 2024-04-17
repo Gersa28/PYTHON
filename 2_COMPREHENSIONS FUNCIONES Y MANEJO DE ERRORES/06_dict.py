f729361a1075b822d14ef10918749a7f2892af34
Germán Salina <gersalina28@gmail.com>
2024-04-17 16:16:49 -0300

    Update 06_dict_continuacion.py

--- 2_COMPREHENSIONS FUNCIONES Y MANEJO DE ERRORES/06_dict.py
+++ 2_COMPREHENSIONS FUNCIONES Y MANEJO DE ERRORES/06_dict.py
@@ -1,12 +1,17 @@
 import random
+
+# Lista de países
 countries = ['col', 'mex', 'bol', 'pe']
 
-population_v2 = { country: random.randint(1, 100)  for country in countries}
-print(population_v2)
+# Generar un diccionario de población aleatoria para cada país
+population_v2 = {country: random.randint(1, 100) for country in countries}
+print("Diccionario de población por país:", population_v2)
 
-result = { country: population for (country, population) in population_v2.items() if population > 50 }
-print(result)
+# Filtrar países con población mayor a 50
+result = {country: population for (country, population) in population_v2.items() if population > 50}
+print("Países con población mayor a 50:", result)
 
+# Crear un diccionario de vocales únicas en mayúsculas
 text = 'Hola, soy Nicolas'
-unique = { c: c.upper() for c in text if c in 'aeiou' }
-print(unique)
+unique = {c: c.upper() for c in text if c in 'aeiou'}
+print("Diccionario de vocales únicas en mayúsculas:", unique)
