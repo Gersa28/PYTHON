604687e9ac44bf2acfd38c6dd6cd16dd5ca71add
Germán Salina <gersalina28@gmail.com>
2024-04-17 11:21:52 -0300

    Update 02_crud_set.py

--- 2_COMPREHENSIONS FUNCIONES Y MANEJO DE ERRORES/02_crud_set.py
+++ 2_COMPREHENSIONS FUNCIONES Y MANEJO DE ERRORES/02_crud_set.py
@@ -17,15 +17,16 @@ set_countries.update({'ar', 'ecua', 'pe'})
 print(set_countries)
 
 # remove
-
 set_countries.remove('col')
 print(set_countries)
 
 set_countries.remove('ar')
 set_countries.discard('arg')
 print(set_countries)
+
 set_countries.add('arg')
 print(set_countries)
+
 set_countries.clear()
 print(set_countries)
 print(len(set_countries))
