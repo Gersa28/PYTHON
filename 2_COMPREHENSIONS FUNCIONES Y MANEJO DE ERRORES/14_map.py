54649cc00689b9ed97c61ec317e67cddc5c297f9
Germán Salina <gersalina28@gmail.com>
2024-04-17 18:22:19 -0300

    Update 14_map.py

--- 2_COMPREHENSIONS FUNCIONES Y MANEJO DE ERRORES/14_map.py
+++ 2_COMPREHENSIONS FUNCIONES Y MANEJO DE ERRORES/14_map.py
@@ -1,18 +1,31 @@
+# Definir una lista de números
 numbers = [1, 2, 3, 4]
-numbers_v2 = []
+
+# Utilizar un bucle for para duplicar cada elemento de la lista 'numbers'
+doubled_numbers_for_loop = []
 for i in numbers:
-  numbers_v2.append(i * 2)
+    doubled_numbers_for_loop.append(i * 2)
 
-numbers_v3 = list(map(lambda i: i * 2, numbers))
+# Utilizar la función map junto con una función lambda para duplicar cada elemento de la lista 'numbers'
+doubled_numbers_map_lambda = list(map(lambda i: i * 2, numbers))
 
-print(numbers)
-print(numbers_v2)
-print(numbers_v3)
+# Imprimir las listas originales y las nuevas listas creadas
+print("Lista original de números:", numbers)
+print("Números duplicados (con bucle for):", doubled_numbers_for_loop)
+print("Números duplicados (con map y lambda):", doubled_numbers_map_lambda)
 
+# Definir dos listas de números
 numbers_1 = [1, 2, 3, 4]
 numbers_2 = [5, 6, 7]
 
-print(numbers_1)
-print(numbers_2)
+# Imprimir las dos listas originales
+print("Números 1:", numbers_1)
+print("Números 2:", numbers_2)
+
+# Utilizar la función map junto con una función lambda para sumar elemento por elemento
+# de las dos listas 'numbers_1' y 'numbers_2'
 result = list(map(lambda x, y: x + y, numbers_1, numbers_2))
-print(result)
+
+# Imprimir el resultado de la suma de las dos listas
+print("Resultado de la suma de elementos:", result)
+
