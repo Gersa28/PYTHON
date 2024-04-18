07f42b45c357a1f5fe3b6053a5ab58bbd7057956
Germán Salina <gersalina28@gmail.com>
2024-04-18 18:20:16 -0300

    Update 24_write.py

--- 2_COMPREHENSIONS FUNCIONES Y MANEJO DE ERRORES/24_write.py
+++ 2_COMPREHENSIONS FUNCIONES Y MANEJO DE ERRORES/24_write.py
@@ -1,6 +1,17 @@
+# Abre el archivo 'texs.txt' en modo de escritura y lectura ('w+')
+# Si el archivo no existe, se crea; si existe, se trunca
 with open('./texs.txt', 'w+') as file:
-  for line in file:
-    print(line)
-  file.write('nuevas cosas en este archivo\n')
-  file.write('otra linea\n')
-  file.write(' mas linea\n')
+
+    # Itera sobre cada línea del archivo (en este caso, como se abre en modo de escritura, no hay líneas aún)
+    # Por lo tanto, este bucle no realiza ninguna iteración
+    for line in file:
+        print(line)
+
+    # Escribe una cadena en el archivo, seguida de un salto de línea
+    file.write('nuevas cosas en este archivo\n')
+
+    # Escribe otra cadena en el archivo, seguida de un salto de línea
+    file.write('otra linea\n')
+
+    # Escribe una tercera cadena en el archivo, seguida de un salto de línea
+    file.write('mas linea\n')
