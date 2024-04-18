621ce6a4f16d6a7fb268cc1d8da9ee83d31aaaa0
Germán Salina <gersalina28@gmail.com>
2024-04-18 11:47:02 -0300

    Update 20_iter.py

--- 2_COMPREHENSIONS FUNCIONES Y MANEJO DE ERRORES/20_iter.py
+++ 2_COMPREHENSIONS FUNCIONES Y MANEJO DE ERRORES/20_iter.py
@@ -1,9 +1,14 @@
+# Iteración a través de un rango del 1 al 9 (inclusive)
 for i in range(1, 10):
-  print(i)
+    print(i)
+    # Imprime los números del 1 al 9 en cada iteración del bucle
 
+# Creación de un iterador a partir de un rango del 1 al 3 (inclusive)
 my_iter = iter(range(1, 4))
-print(my_iter)
-print(next(my_iter))
-print(next(my_iter))
-print(next(my_iter))
-print(next(my_iter))
+# Iterador creado a partir de un rango del 1 al 3
+
+print(my_iter)  # Imprime el objeto iterador creado
+print(next(my_iter))  # Imprime el próximo elemento del iterador (1)
+print(next(my_iter))  # Imprime el próximo elemento del iterador (2)
+print(next(my_iter))  # Imprime el próximo elemento del iterador (3)
+print(next(my_iter))  # Genera una excepción StopIteration ya que el iterador está agotado
