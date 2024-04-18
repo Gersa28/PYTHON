499b7a6efc78e2eac8a06d45313ac0fd727b8ab9
Germán Salina <gersalina28@gmail.com>
2024-04-18 17:48:35 -0300

    Update 20_read.py

--- 2_COMPREHENSIONS FUNCIONES Y MANEJO DE ERRORES/20_read.py
+++ 2_COMPREHENSIONS FUNCIONES Y MANEJO DE ERRORES/20_read.py
@@ -1,16 +1,26 @@
-import csv
+import csv  # Importamos el módulo csv para leer archivos CSV
 
 def read_csv(path):
-  with open(path, 'r') as csvfile:
-    reader = csv.reader(csvfile, delimiter=',')
-    header = next(reader)
-    data = []
-    for row in reader:
-      iterable = zip(header, row)
-      country_dict = {key: value for key, value in iterable}
-      data.append(country_dict)
-    return data
+    """
+    Función para leer datos de un archivo CSV y convertirlos en una lista de diccionarios.
+
+    Parámetros:
+    path (str): La ruta del archivo CSV.
+
+    Retorna:
+    list: Una lista de diccionarios que contiene los datos del archivo CSV.
+    """
+    with open(path, 'r') as csvfile:  # Abrir el archivo CSV en modo lectura
+        reader = csv.reader(csvfile, delimiter=',')  # Crear un objeto lector de CSV
+        header = next(reader)  # Leer la fila de encabezado del archivo CSV
+        data = []  # Inicializar una lista vacía para almacenar diccionarios
+        for row in reader:  # Iterar sobre cada fila en el archivo CSV
+            iterable = zip(header, row)  # Asociar cada encabezado con su valor de fila correspondiente
+            country_dict = {key: value for key, value in iterable}  # Crear un diccionario para la fila
+            data.append(country_dict)  # Agregar el diccionario a la lista de datos
+    return data  # Devolver la lista de diccionarios que contiene los datos del archivo CSV
 
 if __name__ == '__main__':
-  data = read_csv('./app/data.csv')
-  print(data[0])
+    # Si el script se ejecuta directamente, leer el archivo CSV e imprimir la primera fila de datos
+    data = read_csv('./app/data.csv')
+    print(data[0])  # Imprimir la primera fila de datos
