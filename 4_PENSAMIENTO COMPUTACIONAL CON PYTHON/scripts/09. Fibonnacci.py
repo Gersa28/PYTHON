19094baa5963dd38c3a2f406ce03460cd2801f7f
Germán Salina <gersalina28@gmail.com>
2024-04-21 11:02:40 -0300

    Update 09. Fibonnacci.py

--- 4_PENSAMIENTO COMPUTACIONAL CON PYTHON/scripts/09. Fibonnacci.py
+++ 4_PENSAMIENTO COMPUTACIONAL CON PYTHON/scripts/09. Fibonnacci.py
@@ -1,10 +1,11 @@
 def fibonacci(n):
     if n == 0 or n == 1:
         return 1
-
+    
     return fibonacci(n - 1) + fibonacci(n - 2)
 
 
 if __name__ == '__main__':
     n = int(input('Escribe un entero: '))
-    print(fibonacci(n))
+    for i in range(0, n, 1):    
+        print(fibonacci(i))
