0b44167c16565f119180d7b9bf711cba5212d6bf
Germán Salina <gersalina28@gmail.com>
2024-08-20 21:34:55 +0000

    "Updates"

--- 1_FUNDAMENTOS PYTHON/16_2_matrix.py
+++ 1_FUNDAMENTOS PYTHON/16_2_matrix.py
@@ -1,7 +1,9 @@
-matrix = [[1,2,3],
-          [4,5,6],
-          [7,8,9]]
+matrix = [[1,2,3], # Fila 0
+          [4,5,6], # Fila 1
+          [7,8,9]] # Fila 2
+
 print(matrix[2][1])
+
 numbers = 1,2,3,4,5
 print(numbers)
 print(type(numbers))
