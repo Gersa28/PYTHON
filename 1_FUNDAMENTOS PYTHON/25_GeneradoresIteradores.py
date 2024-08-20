ff4941edf86f294108ec03afd42f44f60d60a97a
Germán Salina <gersalina28@gmail.com>
2024-08-20 19:44:52 -0300

    Updates 24

--- 1_FUNDAMENTOS PYTHON/25_GeneradoresIteradores.py
+++ 1_FUNDAMENTOS PYTHON/25_GeneradoresIteradores.py
@@ -43,3 +43,13 @@ def mi_generador():
 # Usar el generador
 for valor in mi_generador():
     print(valor)
+
+def fibonacci(limite):
+    a, b = 0, 1
+    while a < limite:
+        yield a
+        a, b = b, a + b
+
+# Usar el generador para la serie de Fibonacci hasta 10
+for numero in fibonacci(10):
+    print(numero)
